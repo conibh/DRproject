6c979dba49c8b0fa28138c49e7ac37957aa157c5
Coni <coni@devs-MacBook-Pro.local>
2023-01-05 17:38:32 +0200

    #

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+const  topBar = document.querySelector('#topBar');
+
+topBar.addEventListener('mouseover', function() {
+    alert('Why are you hovering me');
+});
+   
+
+const toggler = document.querySelector('.navbar-toggler');
+
+toggler.addEventListener('click', function() {
+    console.log('oh oh you so curious');
+});
